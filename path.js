71d194382bdd86f58b22a2713668b86d062cc609
David Badley <badave@gmail.com>
2012-09-26 11:34:16 -0700

    Handles coordinates being passed in

--- path.js
+++ path.js
@@ -1,4 +1,25 @@
-var Path = function() {
-  
+var Path = function(data) {
+  this.parseString = function(string) {
+    // For more detail, see http://www.w3.org/TR/SVG/paths.html
+    // Superfluous white space and separators such as commas can be eliminated (e.g., "M 100 100 L 200 200" 
+    // contains unnecessary spaces and could be expressed more compactly as "M100 100L200 200")
+    // Makes it difficult to parse by white space alone.  
+
+    // Specs indicate that moveto must be the first command
+    if(string[0] != "M") {
+      throw "SVGString needs to begin with an absolute moveto"
+    }
+  }
+
+  if(data instanceof Array) {
+    // see http://stackoverflow.com/questions/5240335/add-two-arrays-without-using-the-concat-method
+    this.push.apply(this, data)
+  } else if(typeof data === "string") {
+    this.parseString(data);
+  }
+
 }
-Path.prototype = []
+Path.prototype = Array()
+Path.prototype.clone = function() {
+
+}
--- tests_qunit.js
+++ tests_qunit.js
@@ -1,5 +1,52 @@
-test("A Path object should ", function() {
+module("inheritance")
+
+test("A path object should ", function() {
   var path = new Path();
   ok( path instanceof Array, "inherit from Array" );
   ok( path instanceof Path, "inherit from Path" );
 });
+
+test("A path object should ", function() {
+  var path = new Path();
+  ok( 'clone' in path, "have a clone method" );
+  ok( 'map' in path, "have a map method" );
+  ok( 'filter' in path, "have a filter method" );
+  ok( 'slice' in path, "have a slice method" );
+});
+
+test("A path object initalized with no data should ", function() {
+  var path = new Path();
+  ok( path.length == 0, "have no coordinates")
+});
+
+module('coordinates')
+
+test("A path object initalized with no coordinates should ", function() {
+  var path = new Path([])
+  ok( path.length == 0, "have no coordinates")
+});
+
+test("A path object initalized with coordinates should ", function() {
+  var coords = [[0, 1]];
+  var path = new Path(coords);
+  ok( path[0] == coords[0], "have those coordinates" );
+  ok( path.length == 1, "have one pair of coordinates");
+
+  var coords = [];
+  for(var i = 0; i < 5; i++) {
+    coords.push([i, i]);
+  }
+  var path = new Path(coords);
+
+  for(var i = 0; i < coords.length; i++) 
+    ok( path[i] == coords[i], "have any arbitrary number of coordinates")
+
+  ok(path.length == coords.length, "have an equal number of arbitrary coordinates")
+});
+
+module("SVG")
+
+test("A path object initialized with an empty string should ", function() {
+  var path = new Path("");
+  ok(path.length == 0, "have no coordinates")
+})
