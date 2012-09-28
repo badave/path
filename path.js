11fb80fc5aeca9e90920d1ea78c5d3e43713f586
David Badley <badave@gmail.com>
2012-09-28 02:32:29 -0700

    Adding more functionality and tests. toString() brings back SVG output. Other methods added in.  Actually has SVG string parsing

--- path.js
+++ path.js
@@ -1,13 +1,46 @@
+Array.prototype.equalTo = function(array) {
+  return !(this < array || array < this);
+}
+
 var Path = function(data) {
-  this.parseString = function(string) {
+  this.parsePath = function(path) {
     // For more detail, see http://www.w3.org/TR/SVG/paths.html
     // Superfluous white space and separators such as commas can be eliminated (e.g., "M 100 100 L 200 200" 
     // contains unnecessary spaces and could be expressed more compactly as "M100 100L200 200")
-    // Makes it difficult to parse by white space alone.  
 
-    // Specs indicate that moveto must be the first command
-    if(string[0] != "M") {
-      throw "SVGString needs to begin with an absolute moveto"
+    // make white space seperating, replace double spaces caused by this replacement with single spaces
+    // and subsequently split this string by spaces
+    var path = path.replace(/([mlzMLZ])/g, "$1 ").replace(/  /g, " ").split(" ");
+
+    // start at 0, 0.  
+    var x = 0;
+    var y = 0;
+    var length = path.length;
+
+    for(var i = 0; i < length; i++) {
+      var command = path[i++];
+
+      if(command == "z" || command == "Z") {
+        if(this[0][0] && this[0][1]) {
+          this.push([this[0][0], this[0][1]]);
+        }
+        break;
+      }
+
+      var new_x = parseInt(path[i++]);
+      var new_y = parseInt(path[i]);
+
+      // This is really simplified.
+      // 
+      if(command == "M" || command == "L") {
+        x = new_x;
+        y = new_y;
+        this.push([x, y]);
+      } else if(command == "m" || command == "l") {
+        x += new_x;
+        y += new_y;
+        this.push([x, y]);
+      } 
     }
   }
 
@@ -15,11 +48,42 @@ var Path = function(data) {
     // see http://stackoverflow.com/questions/5240335/add-two-arrays-without-using-the-concat-method
     this.push.apply(this, data)
   } else if(typeof data === "string") {
-    this.parseString(data);
+    this.parsePath(data);
   }
 
 }
-Path.prototype = Array()
-Path.prototype.clone = function() {
 
+Path.prototype = Array();
+
+Path.prototype.toString = function() {
+  var string = ["M"];
+  var clone = this.clone();
+  var coords = clone.shift();
+  string.push(coords[0], coords[1]);
+  while(coords = clone.shift()) {
+    if(clone.length == 0 && this[0].equalTo(this[this.length - 1])) {
+      string.push("Z");
+      break;
+    }
+    string.push("L");
+    string.push(coords[0], coords[1]);
+  }
+
+  return string.join(" ");
+}
+
+Path.prototype.clone = function() {
+  return new Path(this.slice(0));
+}
+
+Path.prototype.map = function() {
+  return new Path(Array.prototype.slice.apply(this, arguments));
+}
+
+Path.prototype.filter = function() {
+  return new Path(Array.prototype.slice.apply(this, arguments));
+}
+
+Path.prototype.slice = function() {
+  return new Path(Array.prototype.slice.apply(this, arguments));
 }
--- tests_qunit.js
+++ tests_qunit.js
@@ -50,3 +50,69 @@ test("A path object initialized with an empty string should ", function() {
   var path = new Path("");
   ok(path.length == 0, "have no coordinates")
 })
+
+test("A path object initialized with an empty string should ", function() {
+  var path = new Path("");
+  ok(path.length == 0, "have no coordinates")
+})
+
+test("A path with a moveto 100 100 should", function() {
+  var path = new Path("M 100 100");
+  deepEqual(path[0], [100, 100], "should have initial coordinates at 100, 100"); // these arrays are not equal with equal data
+  ok(path.length == 1, "have one set of coordinates");
+})
+
+test("A path with a moveto 100 100 and a relative lineto 100 100", function() {
+  var path = new Path("M 100 100 l 200 200");
+  deepEqual(path[0], [100, 100], "should have initial coordinates at 100, 100");
+  deepEqual(path[1], [300, 300], "should have [1] coordinates at 200, 200");
+  ok(path.length == 2);
+});
+
+test("A path with a moveto 100 100 and a relative lineto 100 100, an absolute lineto 200, 300 that's closed", function() {
+  var path = new Path("M 100 100 l 200 200 L 200 300 z");
+  deepEqual(path[0], [100, 100], "should have initial coordinates at 100, 100");
+  deepEqual(path[1], [300, 300], "should have [1] coordinates at 300, 400");
+  deepEqual(path[2], [200, 300], "should have [2] coordinates at 200, 300");
+  deepEqual(path[3], [100, 100], "should end back at the first position");
+  ok(path.length == 4, "should be 4 coordinates in length");
+});
+
+module("methods returned object type")
+test("A cloned path should", function() {
+  var path = new Path([[1, 2]]).clone();
+  deepEqual(path[0], [1, 2], "be a copy");
+  ok(path instanceof Path, "be a Path");
+});
+
+test("The path map method should", function() {
+  var path = new Path([[1, 2]]).map(function(coord) { return coord; })
+  deepEqual(path[0], [1, 2], "map a path, behaving like an array");
+  ok(path instanceof Path, "be a Path");
+})
+
+test("The filter method should", function() {
+  var path = new Path([[1, 2], [2, 3]]).filter(function(coord) { return coord[0][0] == 1; })
+  deepEqual(path[0], [1, 2], "filter a path, behaving like an array");
+  ok(path instanceof Path, "be a Path");
+})
+
+test("The slice method should", function() {
+  var path = new Path([[1, 2], [2, 3]]).slice(1)
+  deepEqual(path[0], [2, 3], "slice a path, behaving like an array");
+  ok(path instanceof Path, "be a Path");
+})
+
+module("String theory")
+
+test("Should be able to get the prototype in string in SVG form", function() {
+  var path = new Path([[1, 2], [2, 3]])
+  ok(path.toString() == "M 1 2 L 2 3", "should become an SVG string")
+});
+
+
+test("Should be able to get the prototype in string in SVG form", function() {
+  var path = new Path([[1, 2], [2, 3], [4, 5], [1, 2]])
+  ok(path.toString() == "M 1 2 L 2 3 L 4 5 Z", "should become an SVG string")
+});
+
